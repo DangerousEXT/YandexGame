8202c0e2c05612bc7ad8e51f1eff889e2fe9eb5b
bbld10w0 <Maksim.Buchelnikov@urfu.me>
2026-03-01 00:34:29 +0500

    MoveSystem - CharacterAuthoring
    CharacterAuthoring for all moving entitys (player + enemys); Start of work for PlayerAuthoring; InputSystem were made here

---
+++ TungWarriors/Assets/Scripts/CharacterAuthoring.cs
@@ -0,0 +1,43 @@
+using Unity.Entities;
+using UnityEngine;
+using Unity.Mathematics;
+using UnityEngine.Rendering;
+using Unity.Physics;
+public struct CharacterMoveDirection : IComponentData
+{
+    public float2 Value;
+}
+
+public struct CharacterMoveSpeed : IComponentData
+{
+    public float Value;
+}
+
+public class CharacterAuthoring : MonoBehaviour
+{
+    public float MoveSpeed;
+    private class Baker : Baker<CharacterAuthoring>
+    {
+        public override void Bake(CharacterAuthoring authoring)
+        {
+            var entity = GetEntity(TransformUsageFlags.Dynamic);
+            AddComponent<CharacterMoveDirection>(entity);
+            AddComponent(entity, new CharacterMoveSpeed()
+            {
+                Value = authoring.MoveSpeed
+            });
+        }
+    }
+}
+
+public partial struct CharacterMoveSystem : ISystem 
+{
+    public void OnUpdate(ref SystemState state)
+    {
+        foreach (var (velocity, direction, speed) in SystemAPI.Query<RefRW<PhysicsVelocity>, CharacterMoveDirection, CharacterMoveSpeed>())
+        {
+            var moveStep2d = direction.Value * speed.Value;
+            velocity.ValueRW.Linear = new float3(moveStep2d, 0f);
+        }
+    }
+}
